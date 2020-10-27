0ff01cc25991477b8fe0a33d3c2ad3c535c72716
luthfi118 <luthfir96@gmail.com>
2020-10-27 13:53:20 +0700

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py4knn",
-    version="0.0.4",
+    version="0.0.5",
     author="Mgs. M. Luthfi Ramadhan",
     author_email="luthfir96@gmail.com",
     description="K-Nearest Neighbors Python Library",
